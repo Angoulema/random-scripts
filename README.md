8975f60b3e6258df90aceb31db6010e84e9435d9
Angoulema <eugenia.labokha@yandex.by>
2023-02-19 12:45:18 +0300

    refactor: refactor function

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ### Random scripts for personal use
 
-to be updated
+## Move files script
+
+Used when there's need to sort files info folders depending on the last part of the file name.
+
+`IDENTIFICATIONS` -  an array with last file parts used for identification AND creating folders.
+
+Function `moveFiles` takes a path to the directory with files as a `string`, checks files inside and moves them to new folders according to the latest part of the file name. Parts of file names are detected according to `DELIMITER` string.
--- src/move-files.ts
+++ src/move-files.ts
@@ -2,21 +2,11 @@ import { resolve, basename, extname } from 'path';
 import fs from 'fs';
 import { move } from 'fs-extra';
 
-const IDENTIFICATIONS = {
-  MOVE_IDENTIFICATION_1: 'example1',
-  MOVE_IDENTIFICATION_2: 'example2',
-  MOVE_IDENTIFICATION_3: 'example3'
-}
+const IDENTIFICATIONS = ['sample1', 'sample2', 'sample3'];
 
-const createDirs = (rootDir: string) => {
-  for (const identificator of Object.values(IDENTIFICATIONS)) {
-    const path = resolve(__dirname, rootDir, identificator);
-    if (!fs.existsSync(path)) {
-      fs.mkdirSync(path);
-      console.log(`Dir ${identificator} created`);
-    }
-  }
-};
+const DELIMITER = '-';
+
+const MIN_FILENAME_PARTS = 2;
 
 function moveFiles(rootDir: string) {
   if (!rootDir) throw new Error('No dir path');
@@ -31,21 +21,20 @@ function moveFiles(rootDir: string) {
     }
     filedRead = files.length;
     if (!filedRead) throw new Error('No files in folder');
-    createDirs(rootDir);
 
     for (const file of files) {
       const ext = extname(file);
-      const base = basename(file, ext).split('-');
+      const base = basename(file, ext).split(DELIMITER);
       const indentPart = base[base.length - 1];
 
-      if (Object.values(IDENTIFICATIONS).includes(indentPart) && base.length >= 2) {
+      if (IDENTIFICATIONS.includes(indentPart) && base.length >= MIN_FILENAME_PARTS) {
         move(resolve(__dirname, rootDir, file), resolve(__dirname, rootDir, indentPart, file), (err) => {
           if (err) {
             console.log(`Error while moving file: ${file}`);
             filesUnmoved += 1;
           }
         })
-      } else if (base.length >= 2) {
+      } else if (base.length >= MIN_FILENAME_PARTS) {
         console.log(`Unspecified file part: ${indentPart}`);
         filesUnmoved += 1;
       }
